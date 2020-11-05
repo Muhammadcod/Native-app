17ba6254a3b6d3edf2fe42a24934d5ad385134e2
adebayo mohammed <adebayomuhammad73@gmail.com>
2020-11-05 23:07:47 +0100

    feat: Writing logic for getDecks, getDeck, saveDeckTitle, etc

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,60 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import {DATA_STORAGE_KEY} from './keys'
+import {decks} from './_DATA'
 
 export async function getDecks() {
     try {
-        const value = await AsyncStorage.getItem('DATA_STORAGE_KEY')
-        console.log(value)
-        if(value !== null) {
-            // TODO value previously stored
+        const value = await AsyncStorage.getItem(DATA_STORAGE_KEY)
+        if (value === null) {
+            await AsyncStorage.setItem(DATA_STORAGE_KEY, JSON.stringify(decks))
+        } else {
+            return decks
         }
-    } catch(e) {
-        // TODO error reading value
+        console.log(value)
+        return JSON.parse(value)
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export async function getDeck(id) {
+    try {
+        const value = await AsyncStorage.getItem(DATA_STORAGE_KEY)
+        if (value !== null) {
+            console.log('deck.id', JSON.parse(value)[id])
+            return JSON.parse(value)[id]
+        }
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export async function saveDeckTitle(title) {
+    try {
+        let newDeck;
+        newDeck = await AsyncStorage.mergeItem(DATA_STORAGE_KEY, JSON.stringify({
+            [title]: {
+                title: "",
+                questions: [],
+            }
+        }));
+       return newDeck
+
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+export  async function addCardToDeck(title, card) {
+    try {
+        let newCard;
+        newCard = await AsyncStorage.mergeItem(DATA_STORAGE_KEY, JSON.stringify({
+            [title]: {
+                questions: [...decks.questions].concat(card),
+            }
+        }));
+        return newCard
+    } catch (e) {
+        console.log(e)
     }
 }
