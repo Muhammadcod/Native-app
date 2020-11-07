5eaa584deb560bc522f468fda664b34a5561cb3d
adebayo mohammed <adebayomuhammad73@gmail.com>
2020-11-07 23:43:51 +0100

    feat: Implementing tab navigation for home screen

--- App.js
+++ App.js
@@ -1,29 +1,76 @@
+import 'react-native-gesture-handler'
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Platform } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
+import { NavigationContainer } from '@react-navigation/native'
+import { Ionicons, FontAwesome } from '@expo/vector-icons'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs'
 import reducer from './reducers'
-// import DeckList from './components/DeckList'
-import DeckCard from './components/DeckCard'
+// import DeckCard from './components/DeckCard'
+import DeckList from './components/DeckList'
+import AddDeck from './components/AddDeck'
+import { white, purple } from './utils/colors'
+
+const TabNavigator = {
+  Home: {
+    name: 'Deck List',
+    component: DeckList,
+    options: {
+      tabBarIcon: ({ tintColor }) => (
+        <Ionicons name="ios-bookmarks" size={30} color={tintColor} />
+      ),
+      title: 'Decks',
+    },
+  },
+  AddDeck: {
+    name: 'Add Deck',
+    component: AddDeck,
+    options: {
+      tabBarIcon: ({ tintColor }) => (
+        <FontAwesome name="plus-square" size={30} color={tintColor} />
+      ),
+      title: 'Add Entry',
+    },
+  },
+}
+
+const TabNavigatorConfig = {
+  navigationOptions: {
+    header: null,
+  },
+  tabBarOptions: {
+    activeTintColor: Platform.OS === 'ios' ? purple : white,
+    style: {
+      height: 56,
+      backgroundColor: Platform.OS === 'ios' ? white : purple,
+      shadowColor: 'rgba(0, 0, 0, 0.24)',
+      shadowOffset: {
+        width: 0,
+        height: 3,
+      },
+      shadowRadius: 6,
+      shadowOpacity: 1,
+    },
+  },
+}
+
+const Tab =
+  Platform.OS === 'ios'
+    ? createBottomTabNavigator()
+    : createMaterialTopTabNavigator()
 
 export default function App() {
   return (
     <Provider store={createStore(reducer)}>
-      <View style={styles.container}>
-        <StatusBar style="auto" />
-        {/* <DeckList /> */}
-        <DeckCard />
-      </View>
+      <NavigationContainer>
+        <Tab.Navigator {...TabNavigatorConfig}>
+          <Tab.Screen {...TabNavigator.Home} />
+          <Tab.Screen {...TabNavigator.AddDeck} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    paddingLeft: '15px',
-    paddingRight: '15px',
-  },
-})
---
+++ components/AddDeck.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+class AddDeck extends Component {
+  render() {
+    return <div></div>
+  }
+}
+
+export default AddDeck
